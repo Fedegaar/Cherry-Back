27e7254c66a0518974bcb95eab25798644830a39
emilioalvarez05 <emilioalvarez_5@hotmail.com>
2022-09-14 11:53:58 -0300

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -79,5 +79,17 @@ router.put("/products-change/:id", async (req, res) => {
       .then((data) => res.status(200).json(data))
       .catch((error) => res.status(404).json({ message: error }));
   });
+
+// RUTA DELETE PARA ELIMINAR UN PRODUCTO
+
+router.delete("/products-delete/:id", async (req, res) => {
+    const { id } = req.params;
+
+    productSchema
+        .remove({ _id: id })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+      });
+
   
 module.exports = router;
